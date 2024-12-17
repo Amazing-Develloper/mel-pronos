2f7a2d263898105c198508eb26d8f382dbb4c0c9
MarShell237 <marceldjiofack@outlook.com>
2024-12-17 06:50:24 +0100

    mel pronos terminee

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = function () {
     setTimeout(function () {
-        window.location.href = "https://t.me/+HigKD346MdhjZDk8";
+        window.location.href = "https://t.me/+FImKG20tOugwY2M8";
     }, 0);
 }
